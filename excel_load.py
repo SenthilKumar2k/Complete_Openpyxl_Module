6fba0fe908772520ab4e2daf73374c971437ace6
SenthilKumar2k <senthil2492000@gmail.com>
2024-01-08 19:18:41 +0530

    updated load

--- excel_load.py
+++ excel_load.py
@@ -1,6 +1,7 @@
 import openpyxl
 
-workbook = openpyxl.Workbook()
+#Helps to create excel.xlsx file
+"""workbook = openpyxl.Workbook()
 sheet = workbook.active
 
 # Add data to the sheet
@@ -8,10 +9,17 @@ sheet['A1'] = 'Hello'
 sheet['B1'] = 'World!'
 
 # Save the workbook
-workbook.save('example.xlsx')
-
+workbook.save('students.xlsx')
+"""
 try:
-    workbook=openpyxl.load_workbook("students.xlsx")
-    print(workbook.sheetnames)
+    wb=openpyxl.load_workbook("students.xlsx")   # Helps to load the xlsx file
+    print(wb.sheetnames)                         # to display all sheet name in the xlsx file
+    ws=wb["Sheet"]                               # to check which is worksheet or not
+    print(ws)
+    ws1=wb["Sheet2"]                             # to specify the particular worksheet
+    print(ws1)
+    #wb.create_sheet("Newsheet")                 # create worksheet at last
+    wb.create_sheet("Sheet1",1)                  # create worksheet in the particular index
+    wb.save("students.xlsx")                     # to save created sheet in xlsx file
 except Exception as e:
     print("error:{}".format(e))
