0635db0e399dd408d843721b5710659624b9853f
SenthilKumar2k <senthil2492000@gmail.com>
2024-01-19 19:24:38 +0530

    write update

--- writeupdate.py
+++ writeupdate.py
@@ -6,4 +6,13 @@ print(ws['B5'].value)   # used to read the values in sheet cell
 ws['B5'].value=471      # used to update the value of particular cell
 ws['A9']="meera"        # helps to write in a particular cell
 ws['B9']=453
+print(ws.cell(row=6,column=2).value) # used to read the value in cell
+ws.cell(row=6,column=2).value=478    # used to update the value of particular cell
+ws.cell(row=1, column=3).value="Double Mark"
+print(ws.cell(row=1,column=3).value) 
+# Used to write or update value in multiple column cell
+for i in range(2,10):
+    new=ws.cell(row=i, column=2).value
+    new_col=new*2
+    ws.cell(row=i, column=3).value=new
 wb.save("students.xlsx")
