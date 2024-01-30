dd312dbf2b35b82d928641d705047c10ac5dfb2e
SenthilKumar2k <senthil2492000@gmail.com>
2024-01-30 21:05:04 +0530

    quotes_remove completed

---
+++ special_char_remove.py
@@ -0,0 +1,16 @@
+import openpyxl
+import re
+
+wb=openpyxl.load_workbook("lifetime_report.xlsx")
+ws=wb['Data']
+
+row_count=1
+while True:
+    row_count+=1
+    first=ws.cell(row=row_count, column=2).value
+    if first is not None:
+        print(re.sub(r'@"','',str(first)))
+        ws.cell(row=row_count, column=2).value=first.strip('"')
+    else:
+        break
+wb.save("lifetime_report.xlsx")
